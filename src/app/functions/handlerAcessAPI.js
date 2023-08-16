10af0c3ef9f5dee433161b0aa7146a5052c0499e
caioLoyer <ricartecaio338@gmail.com>
2023-08-16 00:51:09 -0300

    commit-2

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,33 +1,41 @@
 'use server'
 
-const users = [
+const userList = [
     {
         name:'Marcelino',
         email:'marcelino@teste.com',
         password:'1234' ,
-        token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+        token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
     },
     {
         name:'Caio',
         email:'caio@teste.com',
         password:'4567' ,
-        token:'eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ'
+        token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
     },
     {
         name:'Judas',
         email:'judas@teste.com',
-        password:'8900' ,
-        token:'SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+        password:'8901' ,
+        token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
     }
 ]
 
-const getUserAuthenticated = (user) => {
-
-}
+const getUserAuthenticated = (userValor) => {
+    let userAuth = {}
+        userList.map((user) => {
+            if (user.email == userValor.email && user.password == userValor.password){
+                userAuth = user
+            }
+           }
+          )
+           return userAuth
+         }
 
 const getUsers = () =>{
-        
+        return userList       
 }
+
 export { getUsers, getUserAuthenticated };
 
 
--- src/middleware.js
+++ src/middleware.js
@@ -1,24 +1,27 @@
 'use server'
 import { NextResponse } from "next/server";
 import { validateToken } from "./app/functions/validateToken";
-import { users } from ".app/functions/getUsers"
 
 export const middleware = (request) => {
 
     const token = request.cookies.get('token')?.value;
     const urlLogin = new URL('/', request.url);
     const isTokenValidated = validateToken(token);
+    const urlDash = new URL('/pages/dashboard', request.url);
 
     if (!isTokenValidated || !token) {
         if (request.nextUrl.pathname === '/pages/dashboard') {
             return NextResponse.redirect(urlLogin);
         }
     }
+    if (!isTokenValidated || token) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(urlDash);
+        }
+    }
+    
     NextResponse.next();
 };
 export const config = {
     matcher: ['/', '/pages/dashboard']
 };
-
-listaUser.map()
-
